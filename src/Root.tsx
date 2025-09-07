cb414557dc43845e2e35ea95042262db142eb585
Jihan <suhoon222@outlook.com>
2025-09-07 14:01:48 +0900

    fix: 사용하지 않는 ThemeProvider import 제거
    - ESLint 경고 해결을 위한 사용하지 않는 import 제거
    - Netlify CI 빌드 오류 해결

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { styled } from "styled-components";
 import Header from "./components/Header";
-import { ThemeProvider } from "styled-components";
 
 const RootDiv = styled.div`
   margin: 0;
